5d06769b1eb0ad4372f0c6bd2f3c2e497640578c
Harm van den Hoek <harm.vd.hoek@gmail.com>
2020-04-27 14:49:58 +0200

    Implemented OpenProjector.

--- src/main/java/nl/harm27/obswebsocket/sender/GeneralRequestSender.java
+++ src/main/java/nl/harm27/obswebsocket/sender/GeneralRequestSender.java
@@ -86,4 +86,31 @@ public class GeneralRequestSender extends RequestSender {
         sendRequest(new GetVideoInfo.Request(getNextMessageId()),
                 baseResponse -> responseConsumer.accept((GetVideoInfo.Response) baseResponse));
     }
+
+    /**
+     * Open a projector window or create a projector on a monitor. Requires OBS v24.0.4 or newer.
+     * All parameters for request are optional.
+     *
+     * @param type     Type of projector
+     * @param monitor  Monitor to open the projector on. If -1 or omitted, opens a window.
+     * @param geometry Size and position of the projector window (only if monitor is -1).
+     *                 Encoded in Base64 using Qt's geometry encoding (https://doc.qt.io/qt-5/qwidget.html#saveGeometry).
+     *                 Corresponds to OBS's saved projectors.
+     * @param name     Name of the source or scene to be displayed (ignored for other projector types).
+     * @since v4.8.0
+     */
+    public void openProjector(OpenProjector.Type type, Integer monitor, String geometry, String name, Consumer<OpenProjector.Response> responseConsumer) {
+        OpenProjector.Request request = new OpenProjector.Request(getNextMessageId());
+        if (type != null)
+            request.setType(type);
+        if (monitor != null)
+            request.setMonitor(monitor);
+        if (geometry != null)
+            request.setGeometry(geometry);
+        if (name != null)
+            request.setName(name);
+
+        sendRequest(request,
+                baseResponse -> responseConsumer.accept((OpenProjector.Response) baseResponse));
+    }
 }
