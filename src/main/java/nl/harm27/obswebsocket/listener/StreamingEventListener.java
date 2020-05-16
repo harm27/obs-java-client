321220e79a9a1d6845e2fc7d00c93c4937992b7b
Harm van den Hoek <harm.vd.hoek@gmail.com>
2020-05-16 14:26:00 +0200

    Implemented StreamStatus

--- src/main/java/nl/harm27/obswebsocket/listener/StreamingEventListener.java
+++ src/main/java/nl/harm27/obswebsocket/listener/StreamingEventListener.java
@@ -2,10 +2,7 @@ package nl.harm27.obswebsocket.listener;
 
 import nl.harm27.obswebsocket.api.events.BaseEvent;
 import nl.harm27.obswebsocket.api.events.EventType;
-import nl.harm27.obswebsocket.api.events.streaming.StreamStarted;
-import nl.harm27.obswebsocket.api.events.streaming.StreamStarting;
-import nl.harm27.obswebsocket.api.events.streaming.StreamStopped;
-import nl.harm27.obswebsocket.api.events.streaming.StreamStopping;
+import nl.harm27.obswebsocket.api.events.streaming.*;
 
 import java.util.EnumMap;
 import java.util.Map;
@@ -23,6 +20,7 @@ public abstract class StreamingEventListener implements EventListener {
         supportedEvents.put(STREAM_STARTED, StreamStarted.class);
         supportedEvents.put(STREAM_STOPPING, StreamStopping.class);
         supportedEvents.put(STREAM_STOPPED, StreamStopped.class);
+        supportedEvents.put(STREAM_STATUS, StreamStatus.class);
         return supportedEvents;
     }
 
@@ -40,6 +38,9 @@ public abstract class StreamingEventListener implements EventListener {
                 break;
             case STREAM_STOPPED:
                 streamStopped((StreamStopped) baseEvent);
+                break;
+            case STREAM_STATUS:
+                streamStatus((StreamStatus) baseEvent);
                 break;
             default:
                 throw new IllegalStateException("Unexpected EventType for StreamingEventListener.");
@@ -76,5 +77,13 @@ public abstract class StreamingEventListener implements EventListener {
      * @param streamStopped The received event.
      */
     public void streamStopped(StreamStopped streamStopped) {
+    }
+
+    /**
+     * Implement this method to process StreamStatus events.
+     *
+     * @param streamStatus The received event.
+     */
+    public void streamStatus(StreamStatus streamStatus) {
     }
 }
