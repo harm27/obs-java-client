2ff290a114f8b9a63f94869c4e6f4b3dcb7565e2
Harm van den Hoek <harm.vd.hoek@gmail.com>
2020-10-21 18:15:26 +0200

    Moved shared properties for Event and Request in shared class.

---
+++ websocket-maven-plugin/src/main/java/nl/harm27/obs/websocket/generator/datamodel/shared/ContentDefinition.java
@@ -0,0 +1,50 @@
+package nl.harm27.obs.websocket.generator.datamodel.shared;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public abstract class ContentDefinition extends Definition {
+    @JsonProperty("description")
+    private String description;
+    @JsonProperty("api")
+    private String api;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("category")
+    private String category;
+    @JsonProperty("since")
+    private String since;
+    @JsonProperty("deprecated")
+    private String deprecated;
+    @JsonProperty("returns")
+    private List<Property> returns;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getApi() {
+        return api;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getSince() {
+        return since;
+    }
+
+    public String getDeprecated() {
+        return deprecated;
+    }
+
+    public List<Property> getReturns() {
+        return filteredProperties(returns);
+    }
+}
